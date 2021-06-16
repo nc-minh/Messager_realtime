2d86b1b9f6cc51c09f026219db1ef1377aa78ab5
huuvuno1 <nguyenhuuvuno1@gmail.com>
2021-06-16 15:45:41 +0700

    init project

---
+++ src/test/java/nguyenhuuvu/SocialNetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package nguyenhuuvu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
